06ac595b12dc4a44f3245411e1a1fb26f04c3702
Deepi Garg <48362243+deepigarg@users.noreply.github.com>
2020-04-24 15:19:52 +0530

    Update officer.py

--- API/Stakeholder/officer.py
+++ API/Stakeholder/officer.py
@@ -3,25 +3,25 @@ from API.Stakeholder import selectWrapper, insertUpdateDeleteWrapper
 import json
 
 
-def FileFIR(FIRno, Filno, InsName, Desc):
+def fileFIR(FIRno, Filno, InsName, Desc):
 	'''OFFICER: File FIR'''
 	query = "INSERT into FIR(FIRno, FilingNo, InspectorName, Description) values(%s,%s,%s,%s)"
 	param = (FIRno, Filno, InsName, Desc)
 	return insertUpdateDeleteWrapper(query, param)
 
-def CheckDocStatus(typ):
+def checkDocStatus(typ):
 	'''OFFICER: Check Document upload status'''
 	query = "SELECT * from Pending_Cases where is_Verified = 0 and Type = %s"
 	param = (typ,)
 	return selectWrapper(query, param)
 
-def VerifyDoc(Filno):
+def verifyDoc(Filno):
 	'''OFFICER: Verify Document'''
 	query = "UPDATE Pending_Cases SET is_Verified = 1 where FilingNo = %s"
 	param = (Filno,)
 	return insertUpdateDeleteWrapper(query, param)
 
-def AddHearing(CNR, Prevdate, NextDate, Purpose):
+def addHearing(CNR, Prevdate, NextDate, Purpose):
 	'''OFFICER: Add hearings'''
 	query = "UPDATE Active_Cases SET NextHearing = %s, PrevHearing = %s WHERE CNRno = %s"
 	param = (NextDate, Prevdate, CNR)
@@ -34,7 +34,7 @@ def AddHearing(CNR, Prevdate, NextDate, Purpose):
 	param = (NextDate, CNR, Prevdate, Purpose)
 	return insertUpdateDeleteWrapper(query, param)
 
-def Schedule():
+def schedule():
 	'''OFFICER: Check Schedule'''
 	query = "SELECT * from Active_Cases where NextHearing BETWEEN 'CURDATE() 00:00:00' AND 'CURDATE() 23:59:59'"
 	param = tuple()
