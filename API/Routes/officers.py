7a0085855926b250db004712be43930b4c236b71
Deepi Garg <48362243+deepigarg@users.noreply.github.com>
2020-04-24 15:23:45 +0530

    renamed to convention, added firms, officers route

---
+++ API/Routes/officers.py
@@ -0,0 +1,61 @@
+import inspect
+from flask import request, jsonify
+from API.Routes import dataSource, validateResponse
+
+import API.Stakeholder.officer as officer
+from API import api
+
+@api.route('/officer/fileFIR', methods=['POST'])
+def officer_fileFIR():
+    res = dataSource(request)
+    params = inspect.getargspec(officer.fileFIR).args
+
+    # check params should be in res
+    if (not validateResponse(params, res)):
+        return jsonify({'res': 'missing params'})
+    
+    return officer.fileFIR(**res)
+
+@api.route('/officer/checkDocStatus', methods=['POST'])
+def officer_checkDocStatus():
+    res = dataSource(request)
+    params = inspect.getargspec(officer.checkDocStatus).args
+    
+    # check params should be in res
+    if (not validateResponse(params, res)):
+        return jsonify({'res': 'missing params'})
+    
+    return officer.checkDocStatus(**res)
+
+@api.route('/officer/verifyDoc', methods=['POST'])
+def officer_verifyDoc():
+    res = dataSource(request)
+    params = inspect.getargspec(officer.verifyDoc).args
+    
+    # check params should be in res
+    if (not validateResponse(params, res)):
+        return jsonify({'res': 'missing params'})
+    
+    return officer.verifyDoc(**res)
+
+@api.route('/officer/addHearing', methods=['POST'])
+def officer_addHearing():
+    res = dataSource(request)
+    params = inspect.getargspec(officer.addHearing).args
+    
+    # check params should be in res
+    if (not validateResponse(params, res)):
+        return jsonify({'res': 'missing params'})
+    
+    return officer.addHearing(**res)
+
+@api.route('/officer/schedule', methods=['POST'])
+def officer_schedule():
+    res = dataSource(request)
+    params = inspect.getargspec(officer.schedule).args
+    
+    # check params should be in res
+    if (not validateResponse(params, res)):
+        return jsonify({'res': 'missing params'})
+    
+    return officer.schedule(**res)
